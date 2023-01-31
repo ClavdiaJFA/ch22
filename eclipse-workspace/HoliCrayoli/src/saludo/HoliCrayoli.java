916cc66fb7e7492cfb4b706278597e4897015a47
ClavdiaJFA <claudiajfloresaguilera@gmail.com>
2023-01-31 14:11:10 -0600

    update-java

---
+++ eclipse-workspace/HoliCrayoli/src/saludo/HoliCrayoli.java
@@ -0,0 +1,16 @@
+package saludo;
+
+public class HoliCrayoli {
+
+	public static void main(String[] args) {
+		// Esto es un comentario de línea
+		/*
+		 * Esto es un comentario de bloque 
+		 */
+		
+		System.out.println("Holi Crayoli");
+		System.out.println("Estoy en la CH22");
+
+	}
+
+}
